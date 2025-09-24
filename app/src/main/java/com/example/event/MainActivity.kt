97cea65eb16e239b26000644b53b04e3270c8f32
zndrCode <nzandergab@gmail.com>
2025-09-24 11:31:44 +0800

    update

--- app/src/main/java/com/example/event/MainActivity.kt
+++ app/src/main/java/com/example/event/MainActivity.kt
@@ -14,34 +14,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.event.ui.theme.EventTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EventTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EventTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_login)
     }
 }
